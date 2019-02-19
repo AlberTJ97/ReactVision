abb8ebe30694a1071ac118f3157a62e47f3ea6cd
AlberTJ97 <alu0100949568@ull.edu.es>
2019-02-19 00:27:53 +0000

    Updates

---
+++ precache-manifest.e26c19fa11e5a5d1ba9b2a9fea0aa2d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27bfeb2978ebd96b8410",
+    "url": "/ReactVision/static/js/runtime~main.27bfeb29.js"
+  },
+  {
+    "revision": "ba83883f2e087ca4cfb2",
+    "url": "/ReactVision/static/js/main.ba83883f.chunk.js"
+  },
+  {
+    "revision": "c37e29e1e86ca893836f",
+    "url": "/ReactVision/static/js/1.c37e29e1.chunk.js"
+  },
+  {
+    "revision": "ba83883f2e087ca4cfb2",
+    "url": "/ReactVision/static/css/main.4f292852.chunk.css"
+  },
+  {
+    "revision": "78736b158848244250fc39c0fb8bb6ff",
+    "url": "/ReactVision/index.html"
+  }
+];
